e584ee40499edaa08254270c04c6d7d50caf7097
Aamer Khan <ahk@bolius.dk>
2018-08-21 15:48:41 +0200

    re-renaming router for simplicity

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {
-  BrowserRouter as Router,
-  HashRouter
-} from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom';
+import store from './store/UserStore';
 
 var rootDir = process.env.REACT_APP_COMFORTSCORE_ROOT_DIRECTORY,
     widgetId = process.env.REACT_APP_COMFORTSCORE_WIDGET_ID;
 
 ReactDOM.render((
-  <HashRouter hashType="noslash">
-    <App />
-  </HashRouter>
+  <Router>
+    <App store={store} />
+  </Router>
 ), document.getElementById(widgetId));
 registerServiceWorker();

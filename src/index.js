44599dec434c36f9795b2d24a13e27a26b6e0e58
Benjamin <benjamin@rotendahl.dk>
2019-03-12 16:53:22 +0100

    Setup for heroku

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ import registerServiceWorker from './registerServiceWorker';
 import { HashRouter as Router } from 'react-router-dom';
 import store from './store/UserStore';
 
-var rootDir = process.env.REACT_APP_COMFORTSCORE_ROOT_DIRECTORY,
-    widgetId = process.env.REACT_APP_COMFORTSCORE_WIDGET_ID;
-
 ReactDOM.render((
   <Router hashType="noslash">
     <App store={store} />
   </Router>
-), document.getElementById(widgetId));
+), document.getElementById('root'));
 registerServiceWorker();
